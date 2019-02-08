88d887fbdd40716af3ab2e171b8824389d621708
htoth <31545229+htoth18@users.noreply.github.com>
2019-02-08 16:07:38 -0500

    Release 1 Updates
    -Template redesign
    -Footer updates
    -Cursor updates

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -45,3 +45,40 @@ jQuery (document).ready(function(){
      $('.whitebar').removeClass("scroll");
     }
    });
+
+
+  script(type='text/javascript').
+   $('#info').toggle(function() {
+    $('#site-footer').animate({'top': '67px'}, 1000);
+    }, function() {
+    $('#site-footer').animate({'top': '1000px'}, 1000);
+    });
+
+
+script(type='text/javascript').
+   $(function(){
+    $("#info").click(function() {
+     if($('#site-footer').is(':hidden')) {
+      $('#site-footer').css({
+       'display': 'block'
+      });
+      $('#site-footer').slideUp("slow", function() {
+        //animation complete
+      });
+      //$('#site-footer').animate({'top': '67px'}, 1000);
+      //$('.whitebar').addClass("scroll");
+      $('body').css({
+       'overflow': 'hidden'
+      });
+     }
+     else {
+      $('#site-footer').css({
+       'display': 'none'
+      });
+      //$('.whitebar').removeClass("scroll");
+      $('body').css({
+       'overflow': 'auto'
+      });
+     }
+    });
+   }); 
