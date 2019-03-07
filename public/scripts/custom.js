1a95fd40e0c4a682032bd440ceb86257842ad7f0
htoth <31545229+htoth18@users.noreply.github.com>
2019-03-07 10:25:14 -0500

    Clean ups
    -Added a js file to clean up the layout file
    -Fixed slight bug with page animations

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,84 +1,59 @@
 jQuery (document).ready(function(){
     
-    $(function(){
-
-        //animate scroll to footer
-        var footer = $('#site-footer');
-        var bottom = $(window).height() - 
-        $('.scrollLink').click( function() {
-         $('html, body').animate({
-              scrollTop: $('#site-footer').offset().top
-         }, 2000);
-        });
-
-        //set nav bar to become color of footer when at footer
-        var footer = $('#site-footer').offset().top;
-        $(window).scroll(function() {
-            if ($(this).scrollTop() >= footer) {  
-                $('.whitebar').addClass("scroll");
+    /*Google Analytics*/
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-135736418-1');
+    
+    $(function() {
+        $('a').each(function() {
+            if ($(this).attr('id') == 'info') {
+                this.setAttribute("onclick","document.getElementById('site-footer').classList.toggle('closed');ga('send', 'event', '"+$(this).attr("href")+"', 'Click', '"+$(this).text()+"');")
             }
             else {
+                this.setAttribute("onclick","ga('send', 'event', '"+$(this).attr("href")+"', 'Click', '"+$(this).text()+"');")
+            }
+        });
+    });
+    
+    /*page fade-in and fade-out */
+    $('body').css('display','none');
+    $('body').fadeIn(500);
+
+    $(document).on("click", "a", function () {
+        var newUrl = $(this).attr("href");
+        if (!newUrl || newUrl[0] === "#") {
+            location.hash = newUrl;
+            return;
+        }
+        $("html").fadeOut(function () {
+            location = newUrl;
+        });
+        return false;
+    });
+    
+    /*info section animation*/
+    $(function(){
+        $('#info').click(function() {
+            if(!$('#site-footer').hasClass('closed')) {
+                $('#site-footer').animate({'top': '67px'}, 1000);
+                    $(this).delay(1000)
+                           .queue(function () {
+                                $(this).addClass('material-icons').text('arrow_downward');
+                                $(this).dequeue();
+                           });
+                $('.whitebar').delay(1000)
+                              .queue(function() {
+                                $(this).addClass("scroll");
+                                $(this).dequeue();
+                              });
+            }
+            else {
+                $(this).removeClass('material-icons').text('Info');
+                $('#site-footer').animate({'top': '1500px'}, 1000)
                 $('.whitebar').removeClass("scroll");
             }
         });
-        
     });
-    
 });
-
-
-  script(type='text/javascript').
-   var footer = $('#site-footer');
-   var bottom = $(window).height() -
-   $('.scrollLink').click( function() {
-    $('html, body').animate({
-    scrollTop: $('#site-footer').offset().top - 77
-    }, 2000)-1;
-   });
-  script(type='text/javascript').
-   var footer = $('#site-footer').offset().top - 77;
-   $(window).scroll(function() {
-    if ($(this).scrollTop() >= footer) {
-     $('.whitebar').addClass("scroll");
-    }
-    else {
-     $('.whitebar').removeClass("scroll");
-    }
-   });
-
-
-  script(type='text/javascript').
-   $('#info').toggle(function() {
-    $('#site-footer').animate({'top': '67px'}, 1000);
-    }, function() {
-    $('#site-footer').animate({'top': '1000px'}, 1000);
-    });
-
-
-script(type='text/javascript').
-   $(function(){
-    $("#info").click(function() {
-     if($('#site-footer').is(':hidden')) {
-      $('#site-footer').css({
-       'display': 'block'
-      });
-      $('#site-footer').slideUp("slow", function() {
-        //animation complete
-      });
-      //$('#site-footer').animate({'top': '67px'}, 1000);
-      //$('.whitebar').addClass("scroll");
-      $('body').css({
-       'overflow': 'hidden'
-      });
-     }
-     else {
-      $('#site-footer').css({
-       'display': 'none'
-      });
-      //$('.whitebar').removeClass("scroll");
-      $('body').css({
-       'overflow': 'auto'
-      });
-     }
-    });
-   }); 
