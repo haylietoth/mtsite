676763371139e0078804855c2645e53aa0b48e79
hayliet <hayliet@andrew.cmu.edu>
2024-07-17 19:57:30 -0400

    cleaned up scroll behavior conditionals

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -318,35 +318,34 @@ $(window).on("scroll", function() {
    var svgMono = "../images/logos/ME_Black.svg";
    var $svgContainer = $('.logo');
    var windowWidth = $(window).width();
-
-  console.log($(this).scrollTop());
+  // console.log($(this).scrollTop());
 
   if ($(this).scrollTop() <= 10) {
     //hide scroll button
     $('.scrollToTopBtn').removeClass('showBtn');
-
-    //update height of nav and logo on non-mobile only     
-    if (windowWidth >= 768) {
-      $(".whitebar").css('height', '62px');
-        $svgContainer.load(svgFull, function(response, status, xhr) {
-        if (status === 'error') {
-          $(".logo").html("SVG image not found :/");
-        }
-      });
-    }
    } else {
     //show scroll button
     $('.scrollToTopBtn').addClass('showBtn');
+  }
 
-
-    //update height of nav and logo on non-mobile only
-    if (windowWidth >= 768) {
-      $(".whitebar").css('height','55px');
-        $svgContainer.load(svgMono, function(response, status, xhr) {
-        if (status === 'error') {
-          $(".logo").html("SVG image not found :/");
-        }
-      });
+  if (windowWidth >= 768) {
+    if ($(this).scrollTop() <= 1) {
+      $(".whitebar").css('height', '62px');
+      $svgContainer.load(svgFull, function(response, status, xhr) {
+      if (status === 'error') {
+        $(".logo").html("SVG image not found :/");
+      }
+    });
+    } else { 
+      //update height of nav and logo on non-mobile only
+      if (windowWidth >= 768) {
+        $(".whitebar").css('height','55px');
+          $svgContainer.load(svgMono, function(response, status, xhr) {
+          if (status === 'error') {
+            $(".logo").html("SVG image not found :/");
+          }
+        });
+      }
     }
   }
 
